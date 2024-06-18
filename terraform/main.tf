e9a5c6daeff0354e135e09baff4126e78fe6e198
yemmyago <yemmyago@yahoo.com>
2024-06-18 14:48:20 -0500

    testing fix on object id

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,7 +30,8 @@ resource "azurerm_key_vault" "kv" {
   }
   access_policy {
     tenant_id = "aa3ba334-375c-4f89-8679-aacd7f308101"
-    object_id =  azurerm_user_assigned_identity.uami[*].object_id
+    object_id =  azurerm_user_assigned_identity.uami[each.key].principal_id
+
     key_permissions = [
       "Get",
       "List"
